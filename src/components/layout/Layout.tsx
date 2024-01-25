ecc87725c28ccde33cae5da9001d559ac8a05f53
Gustavo René Dias <renegustavo42@gmail.com>
2024-01-25 18:00:47 -0300

    commit initial: starting the project

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,14 @@
+import Footer from "../footer/Footer";
+import Header from "../header/Header";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <div>Content</div>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
